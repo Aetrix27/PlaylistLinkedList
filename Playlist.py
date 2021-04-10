f5b3914c3607c3ce9cf17d936980016103d82d15
Aetrix27 <davedogg323@gmail.com>
2021-04-10 11:40:16 -0700

    Finished getter and setter methods

--- Playlist.py
+++ Playlist.py
@@ -15,12 +15,28 @@ class Playlist:
   # The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index. Otherwise, return -1.
 
   def find_song(self, title):
-      pass
+    current_node = self.__first_song
+    counter = 0
+
+    while current_node != None:
+      counter += 1
+      if title == current_node.get_title():
+        return counter-1
+      else:
+        return -1
 
   # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
 
   def remove_song(self, title):
-    self.__first_song = None
+    current_node = self.__first_song
+    counter = 0
+
+    while current_node != None:
+      counter += 1
+      if title == current_node.get_title():
+        current_node.get_title()
+      else:
+        return -1
 
 
   # TODO: Create a method called length, which returns the number of songs in the playlist.
